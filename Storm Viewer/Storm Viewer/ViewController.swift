30fd88d96edba9c309e1bb553ec2317a5cc2bbc9
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2022-06-11 23:29:42 +0300

    initial commit

---
+++ Storm Viewer/Storm Viewer/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Emirhan KARAHAN on 10.06.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path).sorted()
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.x = indexPath.row + 1
+            vc.y = pictures.count
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+}
