388b8610920bb2483a1d53947c7d5074511bacac
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2022-06-14 17:12:52 +0300

    Add Project 3 - Social Media

--- Storm Viewer/Storm Viewer/DetailViewController.swift
+++ Storm Viewer/Storm Viewer/DetailViewController.swift
@@ -19,6 +19,8 @@ class DetailViewController: UIViewController {
         
         self.title = "Picture \(x!) of \(y!)"
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -35,14 +37,15 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    @objc func shareTapped(){
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("no image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image, selectedImage!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
     
 }
