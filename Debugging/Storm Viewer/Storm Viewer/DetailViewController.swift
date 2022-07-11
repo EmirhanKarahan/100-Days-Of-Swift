e4d1a04f2f18a080ee74e2a1c56c9776744a3b57
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2022-07-11 16:25:15 +0300

    Add Debugging - Day 65

---
+++ Debugging/Storm Viewer/Storm Viewer/DetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Emirhan KARAHAN on 10.06.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var y: Int?
+    var x: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //MARK: - Challenge 2
+        assert(selectedImage == nil, "Selected image is not SELECTED!")
+        
+        self.title = "Picture \(x!) of \(y!)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+}
